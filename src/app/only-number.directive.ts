0ccd090f0f727127f4ccf719cae148900323353c
Gupta <cgupta31@dxc.com>
2020-02-18 14:31:08 +0530

    404 file added

--- src/app/only-number.directive.ts
+++ src/app/only-number.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostListener, ElementRef,Input } from '@angular/core';
+import { Directive, HostListener, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appOnlyNumber]'
@@ -8,9 +8,9 @@ export class OnlyNumberDirective {
   constructor(private _el : ElementRef) { }
 
   @HostListener('input', ['$event']) onInputChange(event) {
-    const initalValue = this._el.nativeElement.value;
-    this._el.nativeElement.value = initalValue.replace(/[^0-9]*/g, '');
-    if ( initalValue !== this._el.nativeElement.value) {
+    const initialValue = this._el.nativeElement.value;
+    this._el.nativeElement.value = initialValue.replace(/[^0-9]*/g, '');
+    if ( initialValue !== this._el.nativeElement.value) {
       event.stopPropagation();
     }
   }
